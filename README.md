33e5bd8b888ed05b4321891aab38fc020ff88068
Mike Wilkes <mikewilkes@outlook.com>
2022-11-27 04:24:36 -0800

    added test for replacing 1 with beep, function partially working

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Test: "It should return an array of numbers from 0 to 'number'"
 Code:
 numberListFunc(2);
 Expected Output: [0,1,2]
+
+Test: A number containing 1 should return "beep" in place of the number
+code:
+numberListFunc(10)
+Expected Output: "beep"
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 function numberListFunc(number) {
   let numArray = [];
-  for ( i = 0; i <= number; i++) {
+  for (let i = 0; i <= number; i++) {
     numArray.push(i);
   }
-  return numArray;
-};
-const numReplace = numArray.map()
+  return numArray.map(number => {
+    if (number.toString().includes('1')) {
+      return 'Beep!';
+    } else {
+      return number;}})
+   
+  };
