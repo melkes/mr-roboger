dec54dc8290f48a267f407b071a274f8607c3dd6
Mike Wilkes <mikewilkes@outlook.com>
2022-12-11 00:15:43 -0800

    WIP trying to make UI work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business Logic
 function numberListFunc(number) {
   let numArray = [];
   for (let i = 0; i <=number; i++){
@@ -15,13 +16,35 @@ function numberListFunc(number) {
     }
   });
 }
+//UI Logic
+window.addEventListener("load", function() {
+  let userNum = document.getElementById('user-number');
+  userNum.innerHTML = inputNumber.value;
+  form.addEventListener("submit", number);
+});
 
+function results(event) {
+  event.preventDefault();
+  const userNum = document.getElementById('user-number');
+  const result = numberListFunc(userNum.value);
+  document.getElementById('output').innerHTML = result;
+}
+
+
+// window.addEventListener("load", function() {
+//   document.querySelector("form#startGame").addEventListener("submit", addPlayers);
+//   document.querySelector("button#roll").addEventListener("click", handleDiceRoll);
+//   document.querySelector("button#hold").addEventListener("click", handleTotal);
+//   document.querySelector("button#playAgainButton").addEventListener("click", handleReset);
+//   // ...................("button#playComputer")...................("click", computerPlayer)
+// });
+
+// I think the biggest issue is that you need to add an event listener for when the window loads like: window.addEventListener("load", function() { ...... }). From within that event listener you can add any other event listeners, grab form data etc.
 // UI logic
 
-document.querySelector("form#user-number").onsubmit = function (event) {
-  event.preventDefault();
-  let userNum = document.getElementById("user-number").value;
+// document.querySelector("form#user-number").onsubmit = function (event) {
+//   event.preventDefault();
+//   let userNum = document.getElementById("user-number").value;
 
-  document.getElementById("output").innerText = userNum;
-};
-
+//   document.getElementById("output").innerText = userNum;
+// };
